c6619495249b6dae0c99ac972ce4ae0090f8054d
CodArs-van <xiaguo.microsoft.com>
2019-05-11 15:53:37 +0000

    tune classifier minor change

--- examples/tune_classifier.py
+++ examples/tune_classifier.py
@@ -2,15 +2,15 @@ import subprocess
 import os
 import sys
 
-lrs = ['2e-5', '3e-5', '4e-5']
-nepochs = ['1', '2', '3']
-bss = ['32', '64', '128', '256']
-msls = ['128', '196', '256', '512']
+lrs = ['2e-5', '3e-5', '5e-5']
+nepochs = ['2', '1', '3']
+bss = ['16', '32', '64']
+msls = ['256', '384']
 
 if sys.argv[1] == '0':
-    msls = ['256', '512']
+    msls = ['256', '384']
 else:
-    msls = ['128', '196']
+    msls = ['512']
 print(msls)
 
 if __name__ == '__main__':
@@ -24,6 +24,6 @@ if __name__ == '__main__':
                         print('exists...skip')
                         continue
                     subprocess.call("python run_classifier.py --task_name jigsaw --do_train --do_lower_case         \
-                            --data_dir /hdfs/input/xiaguo/kgjs19 --bert_model bert-base-uncased --max_seq_length {} \
+                            --data_dir /hdfs/input/xiaguo/ --bert_model bert-base-uncased --max_seq_length {}       \
                             --train_batch_size {} --learning_rate {} --num_train_epochs {}                          \
                             --output_dir {}".format(msl, bs, lr, nepoch, output_dir), shell=True)
