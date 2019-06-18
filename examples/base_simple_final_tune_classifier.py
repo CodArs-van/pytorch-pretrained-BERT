b8d7ea98282e50463ad3a46e64467dd9895be07e
Xiangyu Guo <xiaguo@microsoft.com>
2019-06-17 23:11:55 -0700

    base simple final tune

---
+++ examples/base_simple_final_tune_classifier.py
@@ -0,0 +1,93 @@
+import subprocess
+import os
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# msl, bs, lr, seed
+tune_params = [
+    (384, 512, '3e-5', 1990),
+    (384, 512, '3e-5', 128),
+    (384, 512, '3e-5', 27),
+    (384, 512, '3e-5', 5),
+    (512, 512, '4e-5', 42),
+    (512, 512, '4e-5', 88),
+    (512, 512, '5e-5', 42),
+    (512, 512, '5e-5', 88),
+    (512, 512, '3e-5', 401),
+    (512, 512, '3e-5', 28),
+    (512, 512, '3e-5', 9),
+]
+task = 'jigsaw-b-s'
+n = 3
+
+data_dir = '/hdfs/input/xiaguo/'
+
+if __name__ == '__main__':
+    params = [1]
+    rank = int(sys.argv[1])
+    msl, bs, lr, seed = tune_params[rank]
+
+    for _ in params:
+        name = 'js_base_{}_msl{}_bs{}_lr{}_n{}_sd{}'.format(task, msl, bs, lr.replace('-', ''), n, seed)
+        logger.info('Processing - {}'.format(name))
+
+        output_dir =  os.path.join('.', 'jigsaw-out', name)
+        output_file = '{}.csv'.format(name)
+
+        training = True
+
+        # If exists, skipping...
+        if os.path.exists(output_dir):
+            logger.info('{} exists, skipping training...'.format(output_dir))
+            training = False
+
+        if training:
+            mini = 64 if bs <= 384 else 32
+            assert bs % mini == 0
+            gas = bs // mini
+            # Train toxic classifier
+            ret = subprocess.call("python train_jigsaw19.py --task_name {} --data_dir {}            \
+                --bert_model bert-base-uncased --max_seq_length {} --gradient_accumulation_steps {} \
+                --train_batch_size {} --learning_rate {} --num_train_epochs {} --seed {}            \
+                --output_dir {} --feature_cache_dir {} --use_feature_cache".format(
+                    task, data_dir, msl, gas, bs, lr, n, seed, output_dir, './feature_cache'), shell=True)
+
+            if ret != 0:
+                logger.error("Error train classifier, exit")
+                sys.exit(ret)
+
+        infer_output_dir = os.path.join(output_dir, 'results')
+
+        # Infer toxic comments
+        command = "python infer_jigsaw19.py --do_lower_case --data_dir {} --task_name {} \
+            --max_seq_length {} --infer_batch_size 64".format(data_dir, task, msl)
+        
+        if not os.path.exists(os.path.join(infer_output_dir, output_file)):
+            ret = subprocess.call("{} --bert_model {} --output_dir {} --output_file {}".format(
+                command, output_dir, infer_output_dir, output_file), shell=True)
+
+            if ret != 0:
+                logger.error("Error infer main model")
+                sys.exit(ret)
+        
+        for subdir in [f.name for f in os.scandir(output_dir) if f.is_dir()]:
+            output_subdir = os.path.join(output_dir, subdir)
+            output_subfile = '{}-{}.csv'.format(name, subdir)
+
+            if os.path.exists(os.path.join(infer_output_dir, output_subfile)):
+                logger.info('{} already inferenced...Skip...'.format(output_subdir))
+                continue
+
+            if not os.path.exists(os.path.join(output_subdir, 'config.json')):
+                logger.info('Skipping {}'.format(output_subdir))
+                continue
+
+            ret = subprocess.call("{} --bert_model {} --output_dir {} --output_file {}".format(
+                command, output_subdir, infer_output_dir, output_subfile), shell=True)
+
+            if ret != 0:
+                logger.error("Error infer sub model")
+                sys.exit(ret)
