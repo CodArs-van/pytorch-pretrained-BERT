ee0032d4599ade4d0ef2c615019a241e6d6c79f5
Xiangyu Guo <xiaguo@microsoft.com>
2019-06-22 12:45:37 -0700

    rename arg

--- examples/transform_masklm.py
+++ examples/transform_masklm.py
@@ -9,12 +9,12 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--bert_model", type=str, required=True,
+parser.add_argument("--masklm_model", type=str, required=True,
                     help="The path of pretrained bert model.")
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    path = os.path.join(args.bert_model, "pytorch_model.bin")
+    path = os.path.join(args.masklm_model, "pytorch_model.bin")
     logger.info("path: {}".format(path))
     state_dict = torch.load(path)
     if hasattr(state_dict, "module"):
